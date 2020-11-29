0cba84e6a4ed45f462cdad0ee102627c9fef74bb
gr4viton <lordmutty@gmail.com>
2020-11-29 23:41:37 +0100

    first song format

---
+++ main.py
@@ -0,0 +1,67 @@
+from typing import Optional, List
+
+from fastapi import FastAPI
+
+from pydantic import BaseModel
+from enum import Enum
+
+
+class Chord(BaseModel):
+    letter: str
+
+
+class ChordPoint(BaseModel):
+    x: float
+    chord: Chord
+
+
+class TextAbscissa(BaseModel):
+    text: str
+    x_min: float = 0
+    x_max: float = 1
+    voice_name: str = None
+
+
+class VerseLine(BaseModel):
+    chord_points: List(ChordPoint)  # chord_line
+    text_abscissas: List(TextAbscissa)  # text_abscissas
+
+
+class SectionCategory(Enum):
+    verse = "verse"
+    refrain = "refrain"
+    intro = "intro"
+    bridge = "bridge"
+    outro = "outro"
+
+
+class Section(BaseModel):
+    category: SectionCategory
+    verse_number: int
+    lines: List(VerseLine)
+
+
+class Song(BaseModel):
+    sections: List(Section)
+    name: str
+    language: str
+    author: str
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/songs/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
